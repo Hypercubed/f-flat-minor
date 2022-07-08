9b73bb1ae5dba0528c78bfbf44867f50b9770406
J. Harshbarger <hypercubed@gmail.com>
2022-07-07 23:59:19 -0600

    chomp extension

---
+++ cmp-extension.js
@@ -0,0 +1,10 @@
+Chomp.registerTemplate('cmp', function (task) {
+  if (typeof task.templateOptions.cmp !== 'string')
+    throw new Error('Cmp template expects a filename in template-options.');
+  if (!task.run)
+    throw new Error('Cmp template expects a run field.');
+  return [{
+    ...task,
+    run: `${task.run.trim()} | cmp --s - "${task.templateOptions.cmp}"`
+  }];
+});
