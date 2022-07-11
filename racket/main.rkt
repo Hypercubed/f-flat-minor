3eb28867d11c3ccb8e4dd0780fc4dec93c87689a
J. Harshbarger <hypercubed@gmail.com>
2022-07-10 21:57:25 -0600

    improve main and make

--- racket/main.rkt
+++ racket/main.rkt
@@ -2,3 +2,25 @@
 
 (require "./src/reader.rkt")
 (provide read-syntax)
+
+(module+ main
+  (require racket/cmdline)
+  (require racket/match)
+
+  (command-line
+    #:args (filename)
+
+    ;; parse file
+    (define module-syntax
+      (call-with-input-file* filename
+        (lambda (in)
+          (read-syntax filename in))))
+
+    ;;; eval module form in a namespace with racket/base
+    (define ns (make-base-namespace))
+    (eval module-syntax ns)
+
+    ;; require the module in the namespace to run it
+    (eval `(require ',(second (syntax->datum module-syntax))) ns)
+  )
+)
