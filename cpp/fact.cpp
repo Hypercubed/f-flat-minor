5149e1e96fb2583b9d46fb412de6107822d5f03f
Jayson Harshbarger <jayson.harshbarger@swimlane.com>
2022-07-18 22:07:36 -0600

    missing file

---
+++ cpp/fact.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <boost/multiprecision/gmp.hpp>
+
+using namespace boost::multiprecision;
+
+mpz_int factorial(mpz_int n) {
+  return n == 0 ? 1 : n * factorial(n - 1);
+}
+
+int main()
+{
+  std::cout << "Factorial 100:" << std::endl;
+  std::cout << "[ " << factorial(100) << " ]" << std::endl;
+}
